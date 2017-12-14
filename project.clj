aaad7acc5810604ad5d707d1e26352ea2837b063
Josh Lehman <jalehman37@gmail.com>
2017-12-14 15:52:36 -0800

    release to clojars

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/starcity-properties/toolbelt-core"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]]
+  :deploy-repositories [["releases" :clojars]])
