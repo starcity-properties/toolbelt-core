3ad24d5691075b156a7e8fe25e23d5dfdb0c0519
Josh Lehman <jalehman37@gmail.com>
2017-12-14 12:56:21 -0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject starcity/toolbelt-core "0.1.0-SNAPSHOT"
+  :description "Core utility functions for Clojure and ClojureScript."
+  :url "https://github.com/starcity-properties/toolbelt-core"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]])
---
+++ test/toolbelt_core/core_test.clj
@@ -0,0 +1,7 @@
+(ns toolbelt-core.core-test
+  (:require [clojure.test :refer :all]
+            [toolbelt-core.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
