081fc2af4be41252c6db24cb24000afc9e156d08
Josh Lehman <jalehman37@gmail.com>
2017-12-14 15:40:20 -0800

    Version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/toolbelt-core "0.1.0"
+(defproject starcity/toolbelt-core "0.2.0-SNAPSHOT"
   :description "Generic utility functions for Clojure/Script."
   :url "https://github.com/starcity-properties/toolbelt-core"
   :license {:name "Eclipse Public License"
